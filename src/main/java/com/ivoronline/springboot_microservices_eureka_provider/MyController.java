c924fcd38e368e2e499cf32bd0e7fc165d350fdc
igoreta <ivoronline@gmail.com>
2021-07-29 15:04:42 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_microservices_eureka_provider/MyController.java
@@ -0,0 +1,14 @@
+package com.ivoronline.springboot_microservices_eureka_provider;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+  @RequestMapping("Hello")
+  String hello() {
+    return "Hello from Provider";
+  }
+
+}
---
+++ src/main/java/com/ivoronline/springboot_microservices_eureka_provider/SpringbootMicroservicesEurekaProviderApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_microservices_eureka_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroservicesEurekaProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootMicroservicesEurekaProviderApplication.class, args);
+  }
+
+}
